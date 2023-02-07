0bad40b73c5e480d0def3656b5994768cda0695f
ubeysaab <aboosh.sa262000@gmail.com>
2023-02-07 15:34:35 +0300

    Updates

---
+++ precache-manifest.06dfc83c506759293c405351374c1453.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "22c03e9917216127fb3e",
+    "url": "/animeBlog-1/static/css/main.115d8465.chunk.css"
+  },
+  {
+    "revision": "22c03e9917216127fb3e",
+    "url": "/animeBlog-1/static/js/main.22c03e99.chunk.js"
+  },
+  {
+    "revision": "0f690c1c7caf79060401",
+    "url": "/animeBlog-1/static/css/1.12cf06f2.chunk.css"
+  },
+  {
+    "revision": "0f690c1c7caf79060401",
+    "url": "/animeBlog-1/static/js/1.0f690c1c.chunk.js"
+  },
+  {
+    "revision": "6622a7a9e5af219160c3",
+    "url": "/animeBlog-1/static/js/runtime~main.6622a7a9.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/animeBlog-1/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/animeBlog-1/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/animeBlog-1/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/animeBlog-1/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "6ad413b6f09f901cfd744540a19f5795",
+    "url": "/animeBlog-1/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/animeBlog-1/precache-manifest.06dfc83c506759293c405351374c1453.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/animeBlog-1/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
